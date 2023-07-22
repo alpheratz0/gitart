6fdd584c36276de98fa012a1c84020e8e0b94373
alpheratz0 <alpheratz99@protonmail.com>
2023-07-22 18:57:25 -0300

    add -c option to change commit message

--- gitart.c
+++ gitart.c
@@ -36,7 +36,7 @@
 #define DEFAULT_INTENSITY 100
 #define DEFAULT_DIR "art"
 #define DEFAULT_TEXT "gitart"
-#define COMMIT_MESSAGE "gitart"
+#define DEFAULT_COMMIT_MESSAGE "gitart"
 
 static void
 die(const char *fmt, ...)
@@ -62,7 +62,9 @@ enotnull(const char *str, const char *name)
 static void
 usage(void)
 {
-	puts("usage: gitart [-hv] [-d dirname] [-i intensity] [-w week_offset] [text]");
+	puts("usage: gitart [-hv] [-d dirname] [-i intensity]");
+	puts("              [-w week_offset] [-c commit_message]");
+	puts("              [text]");
 	exit(0);
 }
 
@@ -74,7 +76,7 @@ version(void)
 }
 
 static void
-add_new_commit(git_repository *repo, git_signature *sig)
+add_new_commit(git_repository *repo, git_signature *sig, const char *commit_message)
 {
 	git_oid tid, pid, cid;
 	size_t nparents;
@@ -111,7 +113,7 @@ add_new_commit(git_repository *repo, git_signature *sig)
 	}
 
 	if (git_commit_create_v(&cid, repo, "HEAD", sig, sig,
-				"UTF-8", COMMIT_MESSAGE, tree, nparents, parent) < 0)
+				"UTF-8", commit_message, tree, nparents, parent) < 0)
 		GIT_ERROR("git_commit_create_v");
 
 	if (NULL != parent)
@@ -122,7 +124,7 @@ add_new_commit(git_repository *repo, git_signature *sig)
 
 static void
 set_pixel(git_repository *repo, git_signature *sig, time_t origin,
-		int x, int y, int intensity)
+		int x, int y, int intensity, const char *commit_message)
 {
 	int day_offset;
 
@@ -130,11 +132,12 @@ set_pixel(git_repository *repo, git_signature *sig, time_t origin,
 	sig->when.time = origin + day_offset * SECONDS_PER_DAY;
 
 	while (intensity-- > 0)
-		add_new_commit(repo, sig);
+		add_new_commit(repo, sig, commit_message);
 }
 
 static void
-gitart(const char *dir, const char *text, int intensity, int week_offset)
+gitart(const char *dir, const char *text, const char *commit_message,
+		int intensity, int week_offset)
 {
 	time_t origin;
 	git_repository *repo;
@@ -162,10 +165,14 @@ gitart(const char *dir, const char *text, int intensity, int week_offset)
 	for (i = 0; i < strlen(text); ++i) {
 		printf("rendering glyph: %c\n", text[i]);
 		glyph = five_by_seven + text[i]*7;
-		for (gy = 0; gy < 7; ++gy)
-			for (gx = 0; gx < 5; ++gx)
-				if (glyph[gy] & (1 << (4 - gx)))
-					set_pixel(repo, sig, origin, caret + gx, gy, intensity);
+		for (gy = 0; gy < 7; ++gy) {
+			for (gx = 0; gx < 5; ++gx) {
+				if (glyph[gy] & (1 << (4 - gx))) {
+					set_pixel(repo, sig, origin, caret + gx, gy,
+							intensity, commit_message);
+				}
+			}
+		}
 		caret += 6;
 	}
 
@@ -177,10 +184,11 @@ gitart(const char *dir, const char *text, int intensity, int week_offset)
 int
 main(int argc, char **argv)
 {
-	const char *dir, *text;
+	const char *dir, *text, *commit_message;
 	int intensity, week_offset;
 
 	week_offset = DEFAULT_WEEK_OFFSET;
+	commit_message = DEFAULT_COMMIT_MESSAGE;
 	intensity = DEFAULT_INTENSITY;
 	dir = DEFAULT_DIR;
 	text = NULL;
@@ -192,6 +200,7 @@ main(int argc, char **argv)
 				case 'v': version(); break;
 				case 'i': --argc; intensity = atoi(enotnull(*++argv, "intensity")); break;
 				case 'd': --argc; dir = enotnull(*++argv, "dirname"); break;
+				case 'c': --argc; commit_message = enotnull(*++argv, "commit message"); break;
 				case 'w': --argc; week_offset = atoi(enotnull(*++argv, "week offset")); break;
 				default: die("invalid option %s", *argv); break;
 			}
@@ -208,7 +217,7 @@ main(int argc, char **argv)
 	if (intensity < 1)
 		intensity = 1;
 
-	gitart(dir, text, intensity, week_offset);
+	gitart(dir, text, commit_message, intensity, week_offset);
 
 	return 0;
 }
